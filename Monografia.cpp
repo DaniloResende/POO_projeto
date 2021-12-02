96ebae31c8ffde6fe5386d3224ee71936dad6e5f
Gbishoppp <85043143+Gbishoppp@users.noreply.github.com>
2021-12-02 19:09:29 -0300

    Add files via upload

--- Monografia.cpp
+++ Monografia.cpp
@@ -1,9 +1,42 @@
 #include "Monografia.h"
+#include <string>
 
-Monografia::Monografia(/* args */)
+using namespace std;
+
+Monografia::Monografia(/* args */){}
+
+Monografia::~Monografia(){}
+
+void Monografia::set_orientador(string p_orientador)
 {
+    orientador = p_orientador;
+}
+void Monografia::set_curso(string p_curso)
+{
+    curso = p_curso;
 }
 
-Monografia::~Monografia()
+void Monografia::set_instituicao(string p_instituicao)
 {
+    instituicao = p_instituicao;
+}
+void Monografia::set_tema(string p_tema)
+{
+    tema = p_tema;
+}
+string Monografia::get_orientador(string p_orientador)
+{
+    return orientador;
+}
+string Monografia::get_curso(string p_curso)
+{
+    return curso;
+}
+string Monografia::get_instituicao(string p_instituicao)
+{
+    return instituicao;
+}
+string Monografia::get_tema(string p_tema)
+{
+    return tema;
 }
--- Monografia.h
+++ Monografia.h
@@ -7,9 +7,20 @@ using namespace std;
 class Monografia : public Item
 {
 private:
-   
+string orientador;
+string curso;
+string instituicao;
+string tema; 
 public:
    Monografia(/* args */);
    ~Monografia();
-};
+   void set_orientador(string p_orientador);
+   void set_curso(string p_curso);
+   void set_instituicao(string p_instituicao);
+   void set_tema(string p_tema);
+   string get_orientador(string p_orientador);
+   string get_curso(string p_curso);
+   string get_instituicao(string p_instituicao);
+   string get_tema(string p_tema);
 
+};
