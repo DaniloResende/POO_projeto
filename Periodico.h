8774ae34fdc0010c278d10a854155290d64b86f1
Gbishoppp <85043143+Gbishoppp@users.noreply.github.com>
2021-12-01 21:27:59 -0300

    Add files via upload

--- Periodico.h
+++ Periodico.h
@@ -13,8 +13,6 @@ public:
    void set_edicao(string edicao);
    void set_local(string local);
    void set_editora(string editora);
-   void set_emprestado(int emprestado);
-   int data_piblicacao();
 
 };
 
