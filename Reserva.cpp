60c00e28a6f5143f572280b833d4670456bc1dd5
Gbishoppp <85043143+Gbishoppp@users.noreply.github.com>
2021-12-05 18:53:40 -0300

    Add files via upload

--- Reserva.cpp
+++ Reserva.cpp
@@ -1,9 +1,22 @@
 #include "Reserva.h"
+#include<string>
+using namespace std;
 
-Reserva::Reserva(/* args */)
+Reserva::Reserva(/* args */){}
+Reserva::~Reserva(){}
+void Reserva::set_user(Usuario *p_user_reserva)
 {
+    user_reserva = p_user_reserva;
 }
-
-Reserva::~Reserva()
+void Reserva::set_item(Item *p_item_reservado)
 {
+    item_reservado = p_item_reservado;
+}
+string Reserva::get_user()
+{
+    return user_reserva->get_nome();
+}
+string Reserva::get_item()
+{
+    return item_reservado->get_nome();
 }
--- Reserva.h
+++ Reserva.h
@@ -1,8 +1,18 @@
+#include <string>
+#include "Usuario.h"
+
+using namespace std;
+
 class Reserva
 {
 private:
-   /* data */
+    Usuario* user_reserva;
+    Item* item_reservado;
 public:
    Reserva(/* args */);
    ~Reserva();
+   void set_user(Usuario *p_user_reserva);
+   void set_item(Item *p_item_reservado);
+   string get_user();
+   string get_item();
 };
